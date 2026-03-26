35507df54a7c5c6c480e4c3f3c953e4834bf1cdf
md-morsalin10 <mdmorsalinofficial10@gmail.com>
2026-03-26 20:48:41 +0600

    navbar created

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import { CirclePoundSterling } from 'lucide-react';
+import React from 'react';
+import dollar from "../../../assets/dollar.png"
+import logo from "../../../assets/logo.png"
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="flex-1">
+                    <img className='w-12 h-12' src={logo} alt="" />
+                </div>
+                <div className="flex gap-2 justify-center items-center">
+                    <ul className="menu menu-horizontal px-3">
+                        <li><a>Home</a></li>
+                        <li><a>Fixtures</a></li>
+                        <li><a>Teams</a></li>
+                        <li><a>Schedules</a></li>
+                    </ul>
+                    <p className='btn'>
+                        0 Coins
+                        <img src={dollar} alt="" />
+                        </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
